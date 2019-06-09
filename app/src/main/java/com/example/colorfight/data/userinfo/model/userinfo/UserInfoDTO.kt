7798d68b3465a020e599a09db1b6197085f59c79
slowikj <slowikj@student.mini.pw.edu.pl>
2019-06-09 14:44:27 +0200

    UserInfoDTO: remove suffix DTO when converting to JSON

--- app/src/main/java/com/example/colorfight/data/userinfo/model/userinfo/UserInfoDTO.kt
+++ app/src/main/java/com/example/colorfight/data/userinfo/model/userinfo/UserInfoDTO.kt
@@ -2,6 +2,7 @@ package com.example.colorfight.data.userinfo.model.userinfo
 
 import com.example.colorfight.data.userinfo.model.deviceinfo.DeviceInfoDTO
 import com.example.colorfight.data.userinfo.model.location.DeviceLocationDTO
+import com.fasterxml.jackson.annotation.JsonProperty
 
-data class UserInfoDTO(val deviceInfoDTO: DeviceInfoDTO,
-					   val locationDTO: DeviceLocationDTO? = null)
+data class UserInfoDTO(@JsonProperty("deviceInfo") val deviceInfoDTO: DeviceInfoDTO,
+					   @JsonProperty("location") val locationDTO: DeviceLocationDTO? = null)
