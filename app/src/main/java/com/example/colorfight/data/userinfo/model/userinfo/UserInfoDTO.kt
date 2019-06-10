23d00cf36ed92baacf441f0f449210b3598272d2
slowikj <slowikj@student.mini.pw.edu.pl>
2019-06-10 17:18:37 +0200

    UserInfoDTO: remove renaming fields

--- app/src/main/java/com/example/colorfight/data/userinfo/model/userinfo/UserInfoDTO.kt
+++ app/src/main/java/com/example/colorfight/data/userinfo/model/userinfo/UserInfoDTO.kt
@@ -4,5 +4,5 @@ import com.example.colorfight.data.userinfo.model.deviceinfo.DeviceInfoDTO
 import com.example.colorfight.data.userinfo.model.location.DeviceLocationDTO
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class UserInfoDTO(@JsonProperty("deviceInfo") val deviceInfoDTO: DeviceInfoDTO,
-					   @JsonProperty("location") val locationDTO: DeviceLocationDTO? = null)
+data class UserInfoDTO(val deviceInfoDTO: DeviceInfoDTO,
+					   val locationDTO: DeviceLocationDTO? = null)
